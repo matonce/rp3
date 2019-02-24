5391ea6f174fee27dd753c36aded9639ff6c28a1
Maja Tonček <maja.toncek@gmail.com>
2019-02-24 06:44:54 +0100

    popravak

--- projekt_maja_proba/projekt_maja_proba/VjezbeSLevela.cs
+++ projekt_maja_proba/projekt_maja_proba/VjezbeSLevela.cs
@@ -63,7 +63,7 @@ namespace projekt_maja_proba
 
                 for (int i = 0; i < indeksiVjezbi.Count; ++i)
                 {
-                    /*ProsireniButton prošireniButton = new ProsireniButton();
+                    ProsireniButton prošireniButton = new ProsireniButton();
                     prošireniButton.Size = new Size(105, 105);
 
                     prošireniButton.button1.Name = indeksiVjezbi.ElementAt(i).Item3;
@@ -73,27 +73,11 @@ namespace projekt_maja_proba
 
                     prošireniButton.label1.Text = indeksiVjezbi.ElementAt(i).Item5;
 
-                    */
-                    Button button = new Button()
-                    {
-                        BackColor = Color.RosyBrown,
-                        Font = new Font("Microsoft JhengHei", 12F),
-                        ForeColor = Color.Snow,
-                        Name = indeksiVjezbi.ElementAt(i).Item3,
-                        Size = new Size(105, 105),
-                        TabIndex = indeksiVjezbi.ElementAt(i).Item1, // prije je bilo >i<
-                        Text = indeksiVjezbi.ElementAt(i).Item2,
-                        UseVisualStyleBackColor = false
-                    };
-
                     if (indeksiVjezbi.ElementAt(i).Item4 == 0)
-                        button.Enabled = false;
-                        //prošireniButton.Enabled = false;
-
-                    flowLayoutPanel2.Controls.Add(button);
-                    //flowLayoutPanel2.Controls.Add(prošireniButton);
-                    button.Click += new EventHandler(button_click);
-                    //prošireniButton.button1.Click += new EventHandler(button_click);
+                        prošireniButton.Enabled = false;
+                    
+                    flowLayoutPanel2.Controls.Add(prošireniButton);
+                    prošireniButton.button1.Click += new EventHandler(button_click);
                 }
             }
             else
